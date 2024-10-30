5fc4b51c259f6c730a9bbb8ec63c7fe527c8ac62
Svitlana Mykhailiuk <svetiklan@gmail.com>
2024-10-30 07:41:50 +0200

    start task

---
+++ src/components/App/App.jsx
@@ -0,0 +1,19 @@
+import './App.css';
+
+const App = () => {
+  return (
+    <>
+      {/* <Profile
+        name={userData.username}
+        tag={userData.tag}
+        location={userData.location}
+        image={userData.avatar}
+        stats={userData.stats}
+      />
+      <FriendList friends={friends} />
+      <TransactionHistory items={transactions} /> */}
+    </>
+  );
+};
+
+export default App
